178488df2a01a2441edd88a4212950f5f7bae4d3
mshock <shockleyme@gmail.com>
2016-06-03 16:43:45 -0700

    ajax testing

--- assets/refresh.js
+++ assets/refresh.js
@@ -3,20 +3,28 @@ setInterval(function() {
 	var reload = false;
 	$.ajax({
 		url: 'http://overwatchflair.com',
-		type: "GET",
+		type: "get",
 		statusCode: {
 			404: function() {
-				alert( "page not found" );
+				//alert( "page not found" );
 			},
 			304: function() { 
-				alert( "page not updated" );
+				//alert( "page not updated" );
 			},
 			200: function() { 
-				alert("page retrieved!");
+				//alert("page retrieved!");
 				reload = true;
 			},
 		},
-		success: function() {alert('success!')},
-		error: function() {alert('error!')},
+		success: function() {
+			//alert('success!')
+			if(reload) {
+				$('.ticker').text('');
+				$('.scoreboard').text('');
+			}
+		},
+		error: function() {
+			//alert('error!'
+		)},
 	});	
 }, 5000);
